b9fec4c83db61217bfd1db8f9c65a1bd934ef930
Anass El Omari <anas.iramol@gmail.com>
2024-02-28 05:21:10 -0800

    fixing task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,25 @@
 #include "binary_trees.h"
 
 /**
+  * clone_height - Measures the height of a binary tree
+  * @tree: Pointer to the root node of the tree to measure the height of
+  * Return: Height of the tree, 0 if tree is NULL
+  */
+
+size_t clone_height(const binary_tree_t *tree)
+{
+	size_t height_left, height_right;
+
+	if (!tree)
+		return (-1);
+
+	height_left = clone_height(tree->left);
+	height_right = clone_height(tree->right);
+
+	return (1 + (height_left > height_right ? height_left : height_right));
+}
+
+/**
   * binary_tree_is_perfect - Checks if a binary tree is perfect
   * @tree: Pointer to the root node of the tree to check
   * Return: 1 if the tree is perfect, 0 otherwise
@@ -13,8 +32,8 @@ int height_left, height_right;
 if (tree == NULL)
 	return (0);
 
-height_left = binary_tree_height(tree->left);
-height_right = binary_tree_height(tree->right);
+height_left = clone_height(tree->left);
+height_right = clone_height(tree->right);
 
 if (height_left == height_right)
 {
